feb9ca08eb0b8a2f1b5c8e4be93542c38e9685fe
inkn <ink.night@hotmail.com>
2023-04-11 00:32:21 +0800

    fix: 修复所以主题的 footer

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -12,19 +12,11 @@ export const Footer = (props) => {
   })()
 
   return <footer className="z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray ">
-        <DarkModeButton className='text-center pt-4'/>
+      {/* <DarkModeButton/> */}
 
-        <div className="container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm">
-            <div className='text-center'> &copy;{`${copyrightDate}`} {BLOG.AUTHOR}. All rights reserved.</div>
-            <div className="md:p-0 text-center md:text-right text-xs">
-                {/* 右侧链接 */}
-                {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-                {BLOG.BEI_AN && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>)}
-                <span className='dark:text-gray-200 no-underline ml-4'>
-                    Powered by
-                    <a href="https://github.com/tangly1024/NotionNext" className=' hover:underline'> NotionNext {BLOG.VERSION}  </a>
-                </span>
-            </div>
-        </div>
+      <i className='fas fa-copyright' /> 2023 <span> {title} 
+        <br/>
+        <span className='text-xs '>蜀ICP备2023006850号</span></span><br/>
+
     </footer>
 }
